793290e466b18429c0ae0e44b77dcec3e69c363b
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2023-09-21 14:27:49 -0300

    feat: pacote servico
    adicionado pacote para classes de servico

---
+++ NewGo-Back-End-Breno-Andrade/src/main/java/dominio/produto/servico/ProdutoServico.java
@@ -0,0 +1,21 @@
+package dominio.produto.servico;
+
+import aplicacao.produto.dto.ProdutoInsercaoDto;
+import dominio.produto.entidade.Produto;
+import infraestrutura.ProdutoDAO;
+public class ProdutoServico {
+    private ProdutoDAO produtoDAO;
+    public ProdutoServico() {
+    }
+
+    public Produto salvarNovoProduto(ProdutoInsercaoDto produtoDto){
+
+
+    }
+
+
+    public void verificaNomeDuplicado(String nome){
+        produtoDAO.
+    }
+
+}
