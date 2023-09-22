d71793e2f73f15b2adfbb00076ee5d49094fefb5
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2023-09-22 11:33:00 -0300

    fix: adicionado atributo estoque_min.

--- NewGo-Back-End-Breno-Andrade/src/main/java/aplicacao/produto/dto/ProdutoInsercaoDto.java
+++ NewGo-Back-End-Breno-Andrade/src/main/java/aplicacao/produto/dto/ProdutoInsercaoDto.java
@@ -7,12 +7,15 @@ public class ProdutoInsercaoDto {
     private double preco;
     private double quantidade;
 
-    public ProdutoInsercaoDto(String nome, String descricao, String ean13, double preco, double quantidade) {
+    private double estoque_min;
+
+    public ProdutoInsercaoDto(String nome, String descricao, String ean13, double preco, double quantidade, double estoque_min) {
         this.nome = nome;
         this.descricao = descricao;
         this.ean13 = ean13;
         this.preco = preco;
         this.quantidade = quantidade;
+        this.estoque_min = estoque_min;
     }
 
     public String getNome() {
@@ -33,5 +36,9 @@ public class ProdutoInsercaoDto {
 
     public double getQuantidade() {
         return quantidade;
+    }
+
+    public double getEstoque_min() {
+        return estoque_min;
     }
 }
