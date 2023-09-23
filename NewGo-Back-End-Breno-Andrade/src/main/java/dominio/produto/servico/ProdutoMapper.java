9e1a461544b9dfb3f1adbb190ad4e3234565e3bf
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2023-09-23 20:43:18 -0300

    feat: adicionado classe de mapeamento de produto.

---
+++ NewGo-Back-End-Breno-Andrade/src/main/java/dominio/produto/servico/ProdutoMapper.java
@@ -0,0 +1,34 @@
+package dominio.produto.servico;
+
+import aplicacao.produto.dto.ProdutoInsercaoDto;
+import aplicacao.produto.dto.ProdutoRetornoDto;
+import dominio.produto.entidade.Produto;
+
+public class ProdutoMapper {
+    public Produto insercaoDtoParaEntidade(ProdutoInsercaoDto produtoDto) {
+        return new Produto(
+                produtoDto.getNome(),
+                produtoDto.getDescricao(),
+                produtoDto.getEan13(),
+                produtoDto.getPreco(),
+                produtoDto.getQuantidade(),
+                produtoDto.getEstoque_min()
+        );
+    }
+
+    public ProdutoRetornoDto entidadeParaRetornoDto(Produto produto){
+        return new ProdutoRetornoDto(
+                produto.getId(),
+                produto.getHash(),
+                produto.getNome(),
+                produto.getDescricao(),
+                produto.getEan13(),
+                produto.getPreco(),
+                produto.getQuantidade(),
+                produto.getEstoque_min(),
+                produto.getDtcreate(),
+                produto.getDtupdate(),
+                produto.isLativo()
+        );
+    }
+}
