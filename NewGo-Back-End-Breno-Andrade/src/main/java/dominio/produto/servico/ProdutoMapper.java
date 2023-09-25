2519cb86de2b3aa717316d6aaa4cc57727482eb3
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2023-09-25 20:51:43 -0300

    feat: adicionado metodo para mapeamento passando atributos.
    Please enter the commit message for your changes. Lines starting

--- NewGo-Back-End-Breno-Andrade/src/main/java/dominio/produto/servico/ProdutoMapper.java
+++ NewGo-Back-End-Breno-Andrade/src/main/java/dominio/produto/servico/ProdutoMapper.java
@@ -1,8 +1,12 @@
 package dominio.produto.servico;
 
+import aplicacao.produto.dto.ProdutoAtualizacaoDto;
 import aplicacao.produto.dto.ProdutoInsercaoDto;
 import aplicacao.produto.dto.ProdutoRetornoDto;
 import infraestrutura.produto.entidade.Produto;
+
+import java.sql.Timestamp;
+import java.util.UUID;
 
 public class ProdutoMapper {
     public Produto insercaoDtoParaEntidade(ProdutoInsercaoDto produtoDto) {
@@ -16,6 +20,30 @@ public class ProdutoMapper {
         );
     }
 
+    public Produto atualizacaoDtoParaEntidade(ProdutoAtualizacaoDto produtoDto) {
+        return new Produto(
+                produtoDto.getDescricao(),
+                produtoDto.getPreco(),
+                produtoDto.getQuantidade(),
+                produtoDto.getEstoque_min()
+        );
+    }
+
+    public Produto atualizacaoDtoParaEntidade(long id, UUID hash, String nome, String descricao, String ean13, double preco, double quantidade, double estoque_min, Timestamp dtcreate, Timestamp dtupdate, boolean lativo) {
+        return new Produto(
+                id,
+                hash,
+                nome,
+                descricao,
+                ean13,
+                preco,
+                quantidade,
+                estoque_min,
+                dtcreate,
+                dtupdate,
+                lativo
+        );
+    }
     public ProdutoRetornoDto entidadeParaRetornoDto(Produto produto){
         return new ProdutoRetornoDto(
                 produto.getId(),
