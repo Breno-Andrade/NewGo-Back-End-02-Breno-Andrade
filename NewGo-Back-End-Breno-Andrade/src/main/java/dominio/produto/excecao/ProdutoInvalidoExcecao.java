cb6f55ef81159425936f341813c02bfff086f5e1
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2023-09-25 20:53:32 -0300

    feat: adicionado construtor que recebe erros de atualizacao.

--- NewGo-Back-End-Breno-Andrade/src/main/java/dominio/produto/excecao/ProdutoInvalidoExcecao.java
+++ NewGo-Back-End-Breno-Andrade/src/main/java/dominio/produto/excecao/ProdutoInvalidoExcecao.java
@@ -1,7 +1,13 @@
 package dominio.produto.excecao;
 
+import aplicacao.produto.dto.ProdutoAtualizacaoDto;
+
 public class ProdutoInvalidoExcecao extends IllegalArgumentException {
     public ProdutoInvalidoExcecao(ProdutoInsercaoExcecao mensagemErro) {
+        super(mensagemErro.getMensagem());
+    }
+
+    public ProdutoInvalidoExcecao(ProdutoAtualizacaoExcecao mensagemErro){
         super(mensagemErro.getMensagem());
     }
 }
