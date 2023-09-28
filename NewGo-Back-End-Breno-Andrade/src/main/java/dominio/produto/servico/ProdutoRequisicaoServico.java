1729994fd1d4a07e3504f62a5cb3d22765db8eee
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2023-09-28 10:44:52 -0300

    feat: adiconado servico para requisicoes get.

---
+++ NewGo-Back-End-Breno-Andrade/src/main/java/dominio/produto/servico/ProdutoRequisicaoServico.java
@@ -0,0 +1,31 @@
+package dominio.produto.servico;
+
+import aplicacao.produto.dto.ProdutoRequisicaoDto;
+import aplicacao.produto.dto.ProdutoRetornoDto;
+import dominio.produto.excecao.ProdutoInvalidoExcecao;
+import dominio.produto.excecao.ProdutoRequisicaoExcecao;
+import infraestrutura.produto.dao.ProdutoDAO;
+
+import java.util.UUID;
+
+public class ProdutoRequisicaoServico{
+    private ProdutoDAO produtoDAO = new ProdutoDAO();
+    private ProdutoMapper produtoMapper = new ProdutoMapper();
+
+    public ProdutoRetornoDto requisitarProduto(ProdutoRequisicaoDto produtoDto){
+        try{
+            verificarHash(produtoDto.getHash());
+
+            return produtoMapper.entidadeParaRetornoDto(produtoDAO.buscarPorHash(produtoDto.getHash()));
+        } catch (ProdutoInvalidoExcecao e){
+            throw new ProdutoInvalidoExcecao(ProdutoRequisicaoExcecao.PRODUTO_NAO_ENCONTRADO);
+        }
+    }
+
+    public void verificarHash(UUID hash) throws ProdutoInvalidoExcecao{
+        if(!produtoDAO.existeHash(hash)){
+            throw new ProdutoInvalidoExcecao(ProdutoRequisicaoExcecao.PRODUTO_NAO_ENCONTRADO);
+        }
+    }
+
+}
