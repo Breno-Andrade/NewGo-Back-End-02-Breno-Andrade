742fbbe0ef258bd5ae3846862954f686b91b686f
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2023-09-28 10:42:26 -0300

    feat: adicionado construtor para excecoes de requisicao get.

--- NewGo-Back-End-Breno-Andrade/src/main/java/dominio/produto/excecao/ProdutoInvalidoExcecao.java
+++ NewGo-Back-End-Breno-Andrade/src/main/java/dominio/produto/excecao/ProdutoInvalidoExcecao.java
@@ -2,12 +2,16 @@ package dominio.produto.excecao;
 
 import aplicacao.produto.dto.ProdutoAtualizacaoDto;
 
-public class ProdutoInvalidoExcecao extends IllegalArgumentException {
+public class ProdutoInvalidoExcecao extends RuntimeException {
     public ProdutoInvalidoExcecao(ProdutoInsercaoExcecao mensagemErro) {
         super(mensagemErro.getMensagem());
     }
 
     public ProdutoInvalidoExcecao(ProdutoAtualizacaoExcecao mensagemErro){
+        super(mensagemErro.getMensagem());
+    }
+
+    public ProdutoInvalidoExcecao(ProdutoRequisicaoExcecao mensagemErro){
         super(mensagemErro.getMensagem());
     }
 }
